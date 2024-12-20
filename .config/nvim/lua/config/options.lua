d46024319fa2176143ab36b70a5243b9925e8e7c
Seine Michael <seinemichael223@gmail.com>
2024-12-20 14:50:05 +0800

    "Uses LazyVim"

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,16 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.encoding = "utf-8"
+vim.opt.statuscolumn = ""
+vim.opt.relativenumber = false
+vim.opt.nu = true
+vim.opt.mouse = "a"
+vim.opt.cursorline = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+    { "folke/todo-comments.nvim", enabled = false },
+    { "akinsho/bufferline.nvim", enabled = false },
+    { "folke/flash.nvim", enabled = false },
+    { "folke/trouble.nvim", enabled = false },
+    { "folke/noice.nvim", enabled = false },
+    { "folke/persistence.nvim", enabled = false },
+}
