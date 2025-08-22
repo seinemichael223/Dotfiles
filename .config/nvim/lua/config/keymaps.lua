ddf4e879248f2423d6e9458b5479a740607232d4
Seine Michael <seinemichael223@gmail.com>
2025-08-22 13:29:02 +0800

    nvim updated, flutter, mocha plugins

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Select and replace:
 vim.keymap.set("n", "S", ":%s//gI<Left><Left><Left>")
+
+-- Format JSON with jq:
 vim.keymap.set("n", "<leader>cj", ":%!jq .<CR>", { noremap = true, silent = true, desc = "Format JSON with jq" })
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,7 +4,7 @@
 
 vim.opt.encoding = "utf-8"
 vim.opt.statuscolumn = ""
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 vim.opt.nu = true
 vim.opt.mouse = "a"
 vim.opt.cursorline = true
@@ -15,3 +15,14 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.g.autoformat = false
+vim.opt.winbar = "%=%m %f"
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#74c7ec", bold = false })
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#cba6f7", bold = false })
+    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#eba0ac", bold = false })
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#cba6f7", bold = false })
+  end,
+})
