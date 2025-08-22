37426e9f6772ede13e06e7d124ff830827c3a9c7
Seine Michael <seinemichael223@gmail.com>
2025-08-22 11:56:02 +0800

    yazi updated

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -3,10 +3,10 @@ require("full-border"):setup {
 	type = ui.Border.ROUNDED,
 }
 
--- Showsusername and hostname in header:
-Header:children_add(function()
-	if ya.target_family() ~= "unix" then
-		return ""
-	end
-	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
-end, 500, Header.LEFT)
+ -- Showsusername and hostname in header:
+ Header:children_add(function()
+ 	if ya.target_family() ~= "unix" then
+ 		return ""
+ 	end
+ 	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+ end, 500, Header.LEFT)
